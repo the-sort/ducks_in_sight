c9cc27040a1b505db932c66f415ecf7457d733b7
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-08 15:08:20 +0200

    Duck: horizontal flip, animation added

--- scripts/duck.gd
+++ scripts/duck.gd
@@ -6,11 +6,23 @@ var speed = 1000
 
 func _ready() -> void:
 	res =  get_viewport_rect().size
-	dest = Vector2(randf_range(0, res.x), randf_range(0, res.y))
 
 func _physics_process(_delta: float) -> void:
 	velocity =  position.direction_to(dest) * speed
-	if position.distance_to(dest) > 10:
-		move_and_slide()
-	else :
+
+	if position.distance_to(dest) < 10:
 		dest = Vector2(randf_range(0, res.x), randf_range(0, res.y))
+		face_to(position.direction_to(dest))
+		$duck_animated.play("flight")
+	else :
+		move_and_slide()
+
+func face_to(direction : Vector2) ->void:
+	if direction.x < 0 :
+		$duck_animated.flip_h = false
+		
+	elif direction.x > 0 :
+		$duck_animated.flip_h = true
+	
+	#if direction.y < 0 && $duck_animated.rotation_degrees != 45:
+		#$duck_animated.ro

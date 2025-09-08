313a3a6a3fcb4c338b161d7f19e197f9d98598aa
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-08 12:43:12 +0200

    Duck: random movement

--- scripts/duck.gd
+++ scripts/duck.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+
+var res 
+var dest : Vector2
+var speed = 1000
+
+func _ready() -> void:
+	res =  get_viewport_rect().size
+	dest = Vector2(randf_range(0, res.x), randf_range(0, res.y))
+
+func _physics_process(_delta: float) -> void:
+	velocity =  position.direction_to(dest) * speed
+	if position.distance_to(dest) > 10:
+		move_and_slide()
+	else :
+		dest = Vector2(randf_range(0, res.x), randf_range(0, res.y))

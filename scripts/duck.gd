5af5dabe07768f91d84d5a6139c711dd25b7407f
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-08 15:49:07 +0200

    Duck: set_angle function testing

--- scripts/duck.gd
+++ scripts/duck.gd
@@ -20,9 +20,22 @@ func _physics_process(_delta: float) -> void:
 func face_to(direction : Vector2) ->void:
 	if direction.x < 0 :
 		$duck_animated.flip_h = false
+		set_angle(direction, -1)
 		
 	elif direction.x > 0 :
 		$duck_animated.flip_h = true
-	
-	#if direction.y < 0 && $duck_animated.rotation_degrees != 45:
-		#$duck_animated.ro
+		set_angle(direction, 1)
+
+
+func set_angle(direction : Vector2, flip : int = 1) -> void :
+	if (flip != 1 && flip != -1):
+		push_error("Invalid flip value, set flip value to -1 when object horizontaly fliped")
+		return
+		
+	var angle : float = snappedf( direction.y / 0.022, 1)  * flip
+	if direction.y < 0 :
+		#-----------
+		$duck_animated.rotation_degrees = angle 
+		
+	if direction.x > 0 :
+		$duck_animated.rotation_degrees = angle 

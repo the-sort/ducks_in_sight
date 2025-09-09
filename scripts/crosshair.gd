f6c862d8422840b790fc9faf0c555e96565b3713
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-09 12:32:38 +0200

    cross: crosshair point and click folowing

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+const speed : int = 1000
+var dest : Vector2
+
+func _input(event: InputEvent) -> void:
+	if MOUSE_BUTTON_LEFT && event.is_pressed():
+		dest = get_global_mouse_position()
+
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2.ZERO
+	if position.distance_to(dest) > 10 :
+		velocity = position.direction_to(dest) * speed
+	move_and_slide()
+	pass

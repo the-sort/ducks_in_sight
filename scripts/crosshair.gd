9253688bfdd10ecb3a90fbe7bed60f580e1263e1
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-09 18:20:10 +0200

    Cross: cross no longer slides off

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -2,14 +2,22 @@ extends CharacterBody2D
 
 const speed : int = 1000
 var dest : Vector2
+signal shot
+
+func _ready() -> void:
+	dest = position
 
 func _input(event: InputEvent) -> void:
 	if MOUSE_BUTTON_LEFT && event.is_pressed():
 		dest = get_global_mouse_position()
+	
+	if event.is_action_pressed("shot"):
+		shot.emit()
 
 func _physics_process(_delta: float) -> void:
+
 	velocity = Vector2.ZERO
 	if position.distance_to(dest) > 10 :
 		velocity = position.direction_to(dest) * speed
-	move_and_slide()
-	pass
+		move_and_slide()
+	

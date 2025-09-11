2833e709b6cca978a8be3fa0b5c89112a36e9bda
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-11 15:37:23 +0200

    Cross: shooting mechanic implemented

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 const speed : int = 1000
 var dest : Vector2
-signal shot
+var in_sight : Dictionary[int , Node2D]
+
 
 func _ready() -> void:
 	dest = position
@@ -12,12 +13,17 @@ func _input(event: InputEvent) -> void:
 		dest = get_global_mouse_position()
 	
 	if event.is_action_pressed("shot"):
-		shot.emit()
+		for key in in_sight :
+			in_sight[key].queue_free()
 
 func _physics_process(_delta: float) -> void:
-
 	velocity = Vector2.ZERO
 	if position.distance_to(dest) > 10 :
 		velocity = position.direction_to(dest) * speed
 		move_and_slide()
-	
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	in_sight.set(body.get_instance_id(), body)
+
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	in_sight.erase(body.get_instance_id())

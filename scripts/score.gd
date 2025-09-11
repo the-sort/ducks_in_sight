9e63fa77516033afce3e792a1a19a0cbb556cd10
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-11 20:06:59 +0200

    hud: moved hud to standalone scene

--- scripts/score.gd
+++ scripts/score.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var score_i : int = 0
-@onready var ammo = 10
+@onready var ammo = 5
 
 
 func _ready() -> void:
@@ -9,7 +9,7 @@ func _ready() -> void:
 	$ammo.text =  "Ammo : " + str(ammo)
 
 
-func _on_crosshair_shot(total: int) -> void:
+func _on_crosshair_shot(total: int) -> void: #need to connect to signal
 	if total == 0 :
 		ammo -= 1
 		$ammo.text =  "Ammo : " + str(ammo)

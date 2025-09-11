e78bb91fb69cea2310caa57c40af796bba6823d5
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-11 17:32:54 +0200

    Spawn: initial spawner idea

---
+++ spawner.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var duck_scene = preload("res://scenes/duck.tscn")
+
+
+
+func _on_timeout() -> void:
+	add_child(duck_scene.instantiate())
+	pass # Replace with function body.
+ 

b36afc1247371dc149ce189cb1400109fbcadd87
Filip Dravecký <dravefil@fit.cvut.cz>
2025-09-11 20:30:22 +0200

    Duck: setting limit to duck spawner

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,9 +1,11 @@
 extends Node
 
 @onready var duck_scene = preload("res://scenes/duck.tscn")
-
+@export var limit : int = 10
 
 
 func _on_timeout() -> void:
+	if get_child_count() == limit :
+		return
 	add_child(duck_scene.instantiate())
  

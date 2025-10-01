e737e4b33eef5a22c7d70dfa557e4829a135fc64
Filip Dravecký <dravefil@fit.cvut.cz>
2025-10-01 14:45:11 +0200

    Game_Over: added main_menu button; added textures to buttons

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -5,7 +5,10 @@ func _ready() -> void:
 
 func _on_restart_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
-	
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit(0)
